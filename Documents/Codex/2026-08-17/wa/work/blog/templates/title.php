4f90f020b940f54ec09d570cf5cecbe1c2a5115f
Vasilis Manolakas <vasilismanolakas@gmail.com>
2026-08-17 16:50:38 +0300

    Add title template

---
+++ Documents/Codex/2026-08-17/wa/work/blog/templates/title.php
@@ -0,0 +1,5 @@
+<div class="site-brand">
+    <a class="site-title" href="index.php"><?= html_escape(site_name()) ?></a>
+    <p class="site-tagline"><?= html_escape(site_tagline()) ?></p>
+</div>
+
