8cc155acad85595c9f93315909b1ffa91eeb86a6
Vasilis Manolakas <vasilismanolakas@gmail.com>
2026-08-17 16:50:37 +0300

    Add app config

---
+++ Documents/Codex/2026-08-17/wa/work/blog/src/config.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'site_name' => 'My PHP Blog',
+    'site_tagline' => 'A clean SQLite blog inspired by the I Love PHP tutorial.',
+    'posts_per_page' => 5,
+    'timezone' => 'UTC',
+];
+
